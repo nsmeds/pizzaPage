5271efb5c12a4b65b4c16200a42cf96643f00111
nsmeds <niilosmeds@gmail.com>
2016-01-16 12:36:40 -0800

    Main menu is kinda working but submenus are not. Need to get back button working and append it where appropriate.

--- script.js
+++ script.js
@@ -5,25 +5,50 @@ var hoursdiv = document.getElementById("hoursdiv");
 var about = document.getElementById("about");
 var aboutdiv = document.getElementById("aboutdiv");
 var pizzas = document.getElementById("pizzas");
+var classicsbutton = document.getElementById("classics-button");
+var classics = document.getElementById("classics");
+var signaturesbutton = document.getElementById("signatures-button");
+var signatures = document.getElementById("signatures");
+var buildyourownbutton = document.getElementById("buildyourown-button");
+var buildyourown = document.getElementById("buildyourown");
 var backbutton = document.getElementById("backbutton");
 
-menu.addEventListener("click", pizzamenu);
-hours.addEventListener("click", hours);
-about.addEventListener("click", about);
-backbutton.addEventListener("click", backhome);
 
-function pizzamenu () {
+function pizzamenu() {
     navbar.innerHTML = pizzas.innerHTML;
-};
+}
 
-function hours () {
+function hoursmenu() {
     navbar.innerHTML = hoursdiv.innerHTML;
-};
+}
 
-function about () {
+function aboutmenu() {
     navbar.innerHTML = aboutdiv.innerHTML;
-};
+}
 
-function backhome () {
-    about.innerHTML = navbar.innerHTML;
-};
+function backhome() {
+    pizzas.innerHTML = navbar.innerHTML;
+}
+
+function classicsmenu() {
+    navbar.innerHTML = classics.innerHTML;
+}
+
+function signaturesmenu() {
+    navbar.innerHTML = signatures.innerHTML;
+}
+
+function buildyourownmenu() {
+    navbar.innerHTML = buildyourown.innerHTML;
+}
+
+
+menu.addEventListener("click", pizzamenu);
+hours.addEventListener("click", hoursmenu);
+about.addEventListener("click", aboutmenu);
+classicsbutton.addEventListener("click", classicsmenu);
+signaturesbutton.addEventListener("click", signaturesmenu);
+buildyourownbutton.addEventListener("click", buildyourownmenu);
+hours.addEventListener("click", hoursmenu);
+about.addEventListener("click", aboutmenu);
+backbutton.addEventListener("click", backhome);
