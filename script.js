5e4dabe9fc41c9023a6f4c518fb2c2813307b30a
nsmeds <niilosmeds@gmail.com>
2016-01-18 21:18:32 -0800

    Rewrote JS and menus work better now. Still plenty of content and style to work on.

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
-var navbar = document.getElementById("navbar");
-var menu = document.getElementById("menu");
-var hours = document.getElementById("hours");
-var hoursdiv = document.getElementById("hoursdiv");
-var about = document.getElementById("about");
-var aboutdiv = document.getElementById("aboutdiv");
+var menuTop = document.getElementById("menu-top");
+var hoursButton = document.getElementById("hours-button");
+var menuButton = document.getElementById("menu-button");
+var aboutButton = document.getElementById("about-button");
+var backButton = document.getElementById("back-button");
 var pizzas = document.getElementById("pizzas");
-var classicsbutton = document.getElementById("classics-button");
+var hours = document.getElementById("hours-div");
+var about = document.getElementById("about-div");
+var classicsButton = document.getElementById("classics-button");
+var signaturesButton = document.getElementById("signatures-button");
+var buildYourOwnButton = document.getElementById("buildyourown-button");
 var classics = document.getElementById("classics");
-var signaturesbutton = document.getElementById("signatures-button");
 var signatures = document.getElementById("signatures");
-var buildyourownbutton = document.getElementById("buildyourown-button");
-var buildyourown = document.getElementById("buildyourown");
-var backbutton = document.getElementById("backbutton");
+var buildYourOwn = document.getElementById("build-your-own")
 
+menuButton.addEventListener('click', function(event) {
+    menuTop.style.display = "none";
+    pizzas.style.display = "flex";
+    backButton.style.display = "flex";
+});
 
-function pizzamenu() {
-    navbar.innerHTML = pizzas.innerHTML;
-}
+hoursButton.addEventListener('click', function (event) {
+    menuTop.style.display = "none";
+    hours.style.display = "flex";
+    backButton.style.display = "flex";
+});
 
-function hoursmenu() {
-    navbar.innerHTML = hoursdiv.innerHTML;
-}
+aboutButton.addEventListener('click', function (event) {
+    menuTop.style.display = "none";
+    about.style.display = "flex";
+    backButton.style.display = "flex";
+});
 
-function aboutmenu() {
-    navbar.innerHTML = aboutdiv.innerHTML;
-}
+backButton.addEventListener('click', function (event) {
+    menuTop.style.display = "flex";
+    hours.style.display = '';
+    about.style.display = '';
+    pizzas.style.display = '';
+    classics.style.display = '';
+    signatures.style.display = '';
+    buildYourOwn.style.display = '';
+    backButton.style.display = "flex";
+});
 
-function backhome() {
-    pizzas.innerHTML = navbar.innerHTML;
-}
+classicsButton.addEventListener('click', function(event) {
+    pizzas.style.display = "none";
+    classics.style.display = "flex";
+    backButton.style.display = "flex";
+});
 
-function classicsmenu() {
-    navbar.innerHTML = classics.innerHTML;
-}
+signaturesButton.addEventListener('click', function(event) {
+    pizzas.style.display = "none";
+    signatures.style.display = "flex";
+    backButton.style.display = "flex";
+});
 
-function signaturesmenu() {
-    navbar.innerHTML = signatures.innerHTML;
-}
-
-function buildyourownmenu() {
-    navbar.innerHTML = buildyourown.innerHTML;
-}
-
-
-menu.addEventListener("click", pizzamenu);
-hours.addEventListener("click", hoursmenu);
-about.addEventListener("click", aboutmenu);
-classicsbutton.addEventListener("click", classicsmenu);
-signaturesbutton.addEventListener("click", signaturesmenu);
-buildyourownbutton.addEventListener("click", buildyourownmenu);
-hours.addEventListener("click", hoursmenu);
-about.addEventListener("click", aboutmenu);
-backbutton.addEventListener("click", backhome);
+buildYourOwnButton.addEventListener('click', function(event) {
+    pizzas.style.display = "none";
+    buildYourOwn.style.display = "flex";
+    backButton.style.display = "flex";
+});
